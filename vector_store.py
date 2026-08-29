c0f9703dfc306108903794832fbb003f3808e677
Saibaba <drssaibaba@gmail.com>
2026-08-30 02:38:49 +0530

    Fix production dependency issue for sentence_transformers and langchain_huggingface

--- vector_store.py
+++ vector_store.py
@@ -1,6 +1,6 @@
 import os
 from langchain_community.vectorstores import Chroma
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from data_ingestion import load_and_chunk_data
 
 # Vector Database directory
